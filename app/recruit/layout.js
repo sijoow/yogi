6416edf69f20a96a0b02d9a0d996a7c6b8a016cb
sijoow <leshwann@gmail.com>
2025-07-30 17:22:48 +0900

    sdfs

--- app/recruit/layout.js
+++ app/recruit/layout.js
@@ -1,20 +1,22 @@
 // app/we/layout.js
 
-import { Anton } from 'next/font/google';
+
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import '../we/we.css';
 import '../../components/MainSection.css';
+import { Roboto_Condensed } from 'next/font/google';
 
-const anton = Anton({
-  weight: '400',
-  subsets: ['latin'],
-  variable: '--font-anton', // 필요시 CSS 변수로도 사용 가능
+const roboto = Roboto_Condensed({
+  weight: ['400'],         // 필요에 따라 배열로 여러 웨이트 지정 가능
+  subsets: ['latin'],      // 한글만 쓰신다면 'latin' 하나로 충분합니다
+  variable: '--font-roboto-condensed', // CSS 변수로도 쓰고 싶으면 선언
 });
+
 
 export default function WeLayout({ children }) {
   return (
-    <div className={anton.className}>
+    <div className={`${roboto.className} h-full`}>
       <Navbar />
       {children}
       <Footer />
